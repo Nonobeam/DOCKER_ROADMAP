14819a3404677a44de29ad5e2d613bd73c55703c
Nguyen Huu Phuc <80208064+Nonobeam@users.noreply.github.com>
2023-02-27 11:06:14 +0700

    Add files via upload

---
+++ Day_2/Day_2_springboot_demo/day_2/src/test/java/com/example/day_2/Day2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
