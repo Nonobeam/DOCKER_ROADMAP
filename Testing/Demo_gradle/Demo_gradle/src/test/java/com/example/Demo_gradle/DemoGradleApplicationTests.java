e6f04ed46c9d36ff237f1490a6de1ea8863a999b
Nguyen Huu Phuc <80208064+Nonobeam@users.noreply.github.com>
2023-03-09 11:31:11 +0700

    Add files via upload

---
+++ Testing/Demo_gradle/Demo_gradle/src/test/java/com/example/Demo_gradle/DemoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demo_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
