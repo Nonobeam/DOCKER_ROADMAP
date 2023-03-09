ce2bac0f62b86b55b335bf8fb8ddbc8e796095c4
Nguyen Huu Phuc <80208064+Nonobeam@users.noreply.github.com>
2023-03-09 11:30:31 +0700

    Add files via upload

---
+++ Testing/Demo_maven/src/test/java/com/example/Demo_maven/DemoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demo_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
